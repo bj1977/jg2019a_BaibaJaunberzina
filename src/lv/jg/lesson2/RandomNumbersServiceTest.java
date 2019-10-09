959ccd37978669312a440d152d6a2c9912d40f16
Osis <jaunberzina@gmail.com>
2019-10-09 17:44:35 +0200

    majas darbs

---
+++ src/lv/jg/lesson2/RandomNumbersServiceTest.java
@@ -0,0 +1,30 @@
+package lv.jg.lesson2;
+
+import java.util.Random;
+
+public class RandomNumbersServiceTest {
+    public static void main(String[] args) {
+        Random randomGererator = new Random();
+
+        int  randomNumber1  = randomGererator.nextInt(101);
+
+        System.out.println("Number1-" + randomNumber1);
+
+
+
+        int  randomNumber2  = randomGererator.nextInt(101);
+
+        System.out.println("Number2-" + randomNumber2);
+
+
+        int  randomNumber3  = randomGererator.nextInt(101);
+
+        System.out.println("Number3-" + randomNumber3);
+
+        int sumNumber = randomNumber1 + randomNumber2 + randomNumber3;
+        System.out.println("Randoms Sum is " + sumNumber);
+    }
+}
+
+
+
